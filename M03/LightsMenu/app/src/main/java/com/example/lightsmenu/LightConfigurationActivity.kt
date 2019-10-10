3f273ff2ef3cbcbedaed93f46538509d24c49d69
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-10-10 00:05:37 -0600

    options menu
    create options menu and its functional

--- M03/LightsMenu/app/src/main/java/com/example/lightsmenu/LightConfigurationActivity.kt
+++ M03/LightsMenu/app/src/main/java/com/example/lightsmenu/LightConfigurationActivity.kt
@@ -2,11 +2,18 @@ package com.example.lightsmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_light_configuration.*
 
 class LightConfigurationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_light_configuration)
+
+        title = "Light Configuration"
+
+
+
+        display_name.text = "Light Configuration"
     }
 }
